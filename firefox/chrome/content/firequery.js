669410dd06a90ff8dc20d224e2847706ae198a64
Antonin Hildebrand <antonin@hildebrand.cz>
2010-02-28 00:16:11 +0100

    version bump to 0.7

--- firefox/chrome/content/firequery.js
+++ firefox/chrome/content/firequery.js
@@ -416,7 +416,7 @@ FBL.ns(function() {
         // Firebug.FireQuery
         //
         Firebug.FireQuery = extend(Firebug.ActivableModule, {
-            version: '0.6',
+            version: '0.7',
             /////////////////////////////////////////////////////////////////////////////////////////
             start: function() {
                 dbg(">>>FireQuery.start");
