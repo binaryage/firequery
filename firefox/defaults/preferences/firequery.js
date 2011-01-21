70770ef5f58bafa987e395aca51f1c2b2d8194e3
mikeyw <wolfboy117@yahoo.com>
2011-01-21 13:25:46 -0500

    Modified preferences to always use the latest version of jQuery. Used the non-specific protocol format (//) to allow for http and https connections.

--- firefox/defaults/preferences/firequery.js
+++ firefox/defaults/preferences/firequery.js
@@ -1,6 +1,6 @@
 pref("extensions.firebug.firequery.watcherInterval", 1000);
 
-pref("extensions.firebug.firequery.jQueryURL", 'chrome://firequery-resources/content/jquery.js');
+pref("extensions.firebug.firequery.jQueryURL", '//ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js');
 pref("extensions.firebug.firequery.jQueryURLTimeout", 5000);
 
 pref("extensions.firebug.firequery.jQueryLintURL", 'chrome://firequery-resources/content/jquery.lint.js');
